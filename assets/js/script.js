e3ab1802f89792150f7815b8fb50cca179f2b500
Roan Nascimento Lisboa <142639019+RoanNL@users.noreply.github.com>
2025-04-22 19:09:02 -0300

    Add files via upload
    Implementação do Script da nova animação.

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,22 @@ navLinks.forEach((link) => {
     scroller.scrollTo(href);
   });
 });
+
+
+
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+})
+})
+
+const elements = document.querySelectorAll('.hidden');
+  
+elements.forEach((element) => {
+  myObserver.observe(element);
+})
